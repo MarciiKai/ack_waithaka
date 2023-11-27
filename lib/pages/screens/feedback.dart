c0d6673e5cd60eda0afcbae473a48bbcc62a0413
MarciiKai <kai.marcii2000@gmail.com>
2023-11-27 10:41:01 +0300

    updated

--- lib/pages/screens/feedback.dart
+++ lib/pages/screens/feedback.dart
@@ -1,46 +1,90 @@
+import 'package:ack_waithaka/main.dart';
 import 'package:flutter/material.dart';
 
-class FeedbackScreen extends StatefulWidget{
-  const FeedbackScreen({super.key});
+class ConcaveBackgroundClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final path = Path()
+      ..lineTo(0, size.height - 30)
+      ..quadraticBezierTo(size.width / 2, size.height + 30, size.width, size.height - 30)
+      ..lineTo(size.width, 0)
+      ..close();
+    return path;
+  }
 
   @override
-  // ignore: library_private_types_in_public_api
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
+
+class FeedbackScreen extends StatefulWidget {
+  const FeedbackScreen({Key? key}) : super(key: key);
+
+  @override
   _FeedbackScreenState createState() => _FeedbackScreenState();
 }
 
- class _FeedbackScreenState extends State<FeedbackScreen>{
+class _FeedbackScreenState extends State<FeedbackScreen> {
   final TextEditingController _feedbackController = TextEditingController();
 
-  void _submitFeedback(){
+  void _submitFeedback() {
     // String feedbackText = _feedbackController.text;
   }
- 
-  
-  @override
 
-  Widget build(BuildContext context){
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Feedback Page'), 
+        title: const Text('Feedback Page'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            TextField(
-              controller: _feedbackController,
-              maxLines: 5,
-              decoration: const InputDecoration(
-                labelText: 'Feedback',
-                border: OutlineInputBorder(),
+      body: ClipPath(
+        clipper: ConcaveBackgroundClipper(),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: const Offset(0, 3),
               ),
+            ],
+          ),
+          margin: const EdgeInsets.all(16),
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                TextField(
+                  controller: _feedbackController,
+                  maxLines: 5,
+                  style: TextStyle(fontSize: 16, color: Colors.black),
+                  decoration: InputDecoration(
+                    labelText: 'Feedback',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: _submitFeedback,
+                  style: ElevatedButton.styleFrom(
+                    shadowColor: customColor, // Background color
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  child: const Text('Submit Feedback'),
+                ),
+              ],
             ),
-            const SizedBox(height: 20,),
-            ElevatedButton(onPressed: _submitFeedback, child: const Text('Submit Feedback'))
-          ],
+          ),
         ),
-
-        ),
+      ),
     );
   }
 }
--- lib/pages/screens/register.dart
+++ lib/pages/screens/register.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
 
-class RegistrationScreen extends StatefulWidget {
-  const RegistrationScreen({super.key});
+class ConcaveBackgroundClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final path = Path()
+      ..moveTo(0, size.height)
+      ..quadraticBezierTo(size.width / 3, size.height - 40, size.width / 2, size.height - 20)
+      ..quadraticBezierTo(2 * size.width / 3, size.height, size.width, size.height - 30)
+      ..lineTo(size.width, 0)
+      ..lineTo(0, 0)
+      ..close();
+    return path;
+  }
 
   @override
-  // ignore: library_private_types_in_public_api
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
+
+class RegistrationScreen extends StatefulWidget {
+  const RegistrationScreen({Key? key}) : super(key: key);
+
+  @override
   _RegistrationPageState createState() => _RegistrationPageState();
 }
 
@@ -12,15 +30,15 @@ class _RegistrationPageState extends State<RegistrationScreen> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _phoneController = TextEditingController();
-  String _gender = 'Male'; 
-  String _isAnglican = 'Yes'; 
+  String _gender = 'Male';
+  String _isAnglican = 'Yes';
 
   void _register() {
     String name = _nameController.text;
     String email = _emailController.text;
     String phone = _phoneController.text;
 
- 
+    // Perform registration logic here
   }
 
   @override
@@ -29,108 +47,145 @@ class _RegistrationPageState extends State<RegistrationScreen> {
       appBar: AppBar(
         title: const Text('Church Registration'),
       ),
-      body: Card(
-        elevation: 4,
-     child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextField(
-              controller: _nameController,
-              decoration: const InputDecoration(
-                labelText: 'Name',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: _emailController,
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: _phoneController,
-              keyboardType: TextInputType.phone,
-              decoration: const InputDecoration(
-                labelText: 'Phone',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 20),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text('Gender:'),
-                Row(
-                  children: [
-                    Radio(
-                      value: 'Male',
-                      groupValue: _gender,
-                      onChanged: (value) {
-                        setState(() {
-                          _gender = value.toString();
-                        });
-                      },
-                    ),
-                    const Text('Male'),
-                    Radio(
-                      value: 'Female',
-                      groupValue: _gender,
-                      onChanged: (value) {
-                        setState(() {
-                          _gender = value.toString();
-                        });
-                      },
-                    ),
-                    const Text('Female'),
+      body: Stack(
+        children: [
+          ClipPath(
+            clipper: ConcaveBackgroundClipper(),
+            child: Container(
+              height: 150,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color.fromARGB(255, 214, 165, 156),
+                    Color.fromARGB(255, 235, 208, 149),
                   ],
                 ),
-              ],
+              ),
             ),
-            const SizedBox(height: 20),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text('Are you Anglican?'),
-                Row(
+          ),
+        Padding(padding: EdgeInsets.only(top: 150),
+      child: Center(
+            child: Card(
+              elevation: 8,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Padding(
+                padding:  EdgeInsets.all(24.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Radio(
-                      value: 'Yes',
-                      groupValue: _isAnglican,
-                      onChanged: (value) {
-                        setState(() {
-                          _isAnglican = value.toString();
-                        });
-                      },
+                    TextField(
+                      controller: _nameController,
+                      decoration:  InputDecoration(
+                        labelText: 'Name',
+                        border: OutlineInputBorder(),
+                      ),
                     ),
-                    const Text('Yes'),
-                    Radio(
-                      value: 'No',
-                      groupValue: _isAnglican,
-                      onChanged: (value) {
-                        setState(() {
-                          _isAnglican = value.toString();
-                        });
-                      },
+                    const SizedBox(height: 20),
+                    TextField(
+                      controller: _emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration:  InputDecoration(
+                        labelText: 'Email',
+                        border: OutlineInputBorder(),
+                      ),
                     ),
-                    const Text('No'),
+                    const SizedBox(height: 20),
+                    TextField(
+                      controller: _phoneController,
+                      keyboardType: TextInputType.phone,
+                      decoration: const InputDecoration(
+                        labelText: 'Phone',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text('Gender:'),
+                        Row(
+                          children: [
+                            Radio(
+                              value: 'Male',
+                              groupValue: _gender,
+                              onChanged: (value) {
+                                setState(() {
+                                  _gender = value.toString();
+                                });
+                              },
+                            ),
+                            const Text('Male'),
+                            Radio(
+                              value: 'Female',
+                              groupValue: _gender,
+                              onChanged: (value) {
+                                setState(() {
+                                  _gender = value.toString();
+                                });
+                              },
+                            ),
+                            const Text('Female'),
+                          ],
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text('Are you Anglican?'),
+                        Row(
+                          children: [
+                            Radio(
+                              value: 'Yes',
+                              groupValue: _isAnglican,
+                              onChanged: (value) {
+                                setState(() {
+                                  _isAnglican = value.toString();
+                                });
+                              },
+                            ),
+                            const Text('Yes'),
+                            Radio(
+                              value: 'No',
+                              groupValue: _isAnglican,
+                              onChanged: (value) {
+                                setState(() {
+                                  _isAnglican = value.toString();
+                                });
+                              },
+                            ),
+                            const Text('No'),
+                          ],
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                  ElevatedButton(
+                      onPressed: _register,
+                      style: ElevatedButton.styleFrom(
+                        shadowColor: Color.fromRGBO(241, 203, 105, 1.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        elevation: 5,
+                        fixedSize: Size(200, 50),
+                        padding: const EdgeInsets.symmetric(vertical: 16),
+                      ),
+                      
+                      child: const Text('Register'),
+                    ),
                   ],
                 ),
-              ],
+              ),
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _register,
-              child: const Text('Register'),
-            ),
-          ],
-        ),
-      ),
+          ),
+          ),
+        ],
       ),
     );
   }
