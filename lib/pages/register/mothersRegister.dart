c0ccc58a03a6ef09d59880e99e4f9b333fec9aa9
MarciiKai <kai.marcii2000@gmail.com>
2023-11-27 10:11:24 +0300

    updated

--- lib/pages/register/mothersRegister.dart
+++ lib/pages/register/mothersRegister.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
 
-class MothersRegistrationScreen extends StatefulWidget {
-  const MothersRegistrationScreen({super.key});
+class ConcaveBackgroundClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final path = Path()
+      ..moveTo(0, size.height)
+      ..quadraticBezierTo(size.width / 3, size.height - 60, size.width / 2, size.height - 30)
+      ..quadraticBezierTo(2 * size.width / 3, size.height, size.width, size.height - 20)
+      ..lineTo(size.width, 0)
+      ..lineTo(0, 0)
+      ..close();
+    return path;
+  }
 
   @override
-  // ignore: library_private_types_in_public_api
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
+
+class MothersRegistrationScreen extends StatefulWidget {
+  const MothersRegistrationScreen({Key? key}) : super(key: key);
+
+  @override
   _MothersRegistrationPageState createState() => _MothersRegistrationPageState();
 }
 
@@ -12,53 +30,90 @@ class _MothersRegistrationPageState extends State<MothersRegistrationScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _phoneController = TextEditingController();
 
-
   void _register() {
     String email = _emailController.text;
     String phone = _phoneController.text;
 
- 
+    // Perform registration logic here
+  }
+
+  Widget _buildConcaveBackground() {
+    return ClipPath(
+      clipper: ConcaveBackgroundClipper(),
+      child: Container(
+        height: 550,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color.fromARGB(255, 214, 165, 156), Color.fromARGB(255, 235, 208, 149)],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextField(String label, TextEditingController controller, TextInputType inputType) {
+    return TextField(
+      controller: controller,
+      keyboardType: inputType,
+      decoration: InputDecoration(
+        labelText: label,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        filled: true,
+        fillColor: Colors.white,
+        contentPadding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Mothers Department Registration'),
+        title: Text('Mothers Department Registration'),
       ),
-      body: Card(
-        elevation: 4,
-     child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextField(
-              controller: _emailController,
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(),
+      body: Stack(
+        children: [
+          _buildConcaveBackground(),
+          Center(
+            child: Card(
+              elevation: 8,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    _buildTextField('Email', _emailController, TextInputType.emailAddress),
+                    const SizedBox(height: 16),
+                    _buildTextField('Phone', _phoneController, TextInputType.phone),
+                    const SizedBox(height: 24),
+                    ElevatedButton(
+                      onPressed: _register,
+                      style: ElevatedButton.styleFrom(
+                        shadowColor: Color.fromRGBO(241, 203, 105, 1.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        elevation: 5,
+                        fixedSize: Size(100, 50),
+                        padding: const EdgeInsets.symmetric(vertical: 16),
+                      ),
+                      
+                      child: const Text('Registration Request'),
+                    ),
+                  ],
+                ),
               ),
             ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: _phoneController,
-              keyboardType: TextInputType.phone,
-              decoration: const InputDecoration(
-                labelText: 'Phone',
-                border: OutlineInputBorder(),
-              ),
-            ),
-          
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _register,
-              child: const Text('Registration Request'),
-            ),
-          ],
-        ),
-      ),
+          ),
+        ],
       ),
     );
   }
